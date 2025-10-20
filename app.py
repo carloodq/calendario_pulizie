ba6f48fe514f666a1968a7f10e0f4003898f25d3
Carlo Dalla Quercia <dallaquerciacarlo@gmail.com>
2025-10-20 13:36:26 +0300

    pydantic

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.pydantic_v1 import BaseModel, Field
+from pydantic import BaseModel, Field
 from langchain_anthropic import ChatAnthropic
 from streamlit_calendar import calendar
 from datetime import timedelta
